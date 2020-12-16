72ed8a645ffd02959f1ef139da6187dad74ec801
jskyte <jayson199732@gmail.com>
2020-12-16 15:29:15 +0700

    added PUT and DELETE in Pendidikan

--- src/main/java/com/myproject/demoSpring/controller/PendidikanController.java
+++ src/main/java/com/myproject/demoSpring/controller/PendidikanController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +45,23 @@ public class PendidikanController {
 		return ResponseEntity.ok(pendidikanEntity);
 	}
 	
+	@PutMapping("/update-pendidikan/{idPendidikan}")
+	public ResponseEntity<?> updatePendidikan(@PathVariable Integer idPendidikan, @RequestBody PendidikanDto dto){
+		PendidikanEntity pendidikanEntity = pendidikanRepository.findById(idPendidikan).get();
+		pendidikanEntity.setInstitusi(dto.getInstitusi());
+		pendidikanEntity.setLevel(dto.getLevel());
+		pendidikanEntity.setTahunMasuk(dto.getTahunMasuk());
+		pendidikanEntity.setTahunLulus(dto.getTahunLulus());
+		pendidikanRepository.save(pendidikanEntity);
+		return ResponseEntity.ok(pendidikanEntity);
+	}
+	
+	@DeleteMapping("/delete-pendidikan/{idPendidikan}")
+	public ResponseEntity<?> deletePendidikan(@PathVariable Integer idPendidikan){
+		PendidikanEntity pendidikanEntity = pendidikanRepository.findById(idPendidikan).get();
+		pendidikanRepository.delete(pendidikanEntity);
+		return ResponseEntity.ok(pendidikanEntity);		
+	}
 	
 	// Converter
 	public PendidikanEntity convertToPendidikanEntity(PendidikanDto dto) {
